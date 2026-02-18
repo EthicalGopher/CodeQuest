49ac14ebaa3b7ad09783c4c77c4f03ab30d88b0c
sankhyahrick swami <sankhyahricks@gmail.com>
2026-02-19 00:09:04 +0530

    made changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import './style.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Head } from '@unhead/react';
-import App from './App.jsx';
+import { createHead, UnheadProvider } from '@unhead/react/server'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './style.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <Head />
-        <App />
-    </React.StrictMode>,
+const head = createHead()
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <UnheadProvider value={head}>
+      <App />
+    </UnheadProvider>
+  </StrictMode>,
 )
